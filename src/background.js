3fa5b7731aca2f5c604ae508dc3347e3ef8236bd
sinaesmaeili <sina.esmaeili01@gmail.com>
2018-12-17 14:35:29 -0500

    Further aesthetic and functionality updates

--- src/background.js
+++ src/background.js
@@ -1,20 +1,3 @@
-// chrome.storage.local.set({"urlObjects": [{"test": 123}]}, () => {});
-// chrome.storage.local.get("urlObjects", (result) => {
-//     let arr = [{"test": 123}, {"mandem": 456}];
-//     result.urlObjects.push(arr[1]);
-
-//     chrome.storage.local.set({"urlObjects": result.urlObjects}, () => {
-//         chrome.storage.local.get("urlObjects", (result) => {
-//             console.log('here');
-//             console.log(result.urlObjects);
-//         });
-//     });
-// });
-
-
-
-
-
 let urlObjects = [];
 chrome.storage.local.set({ "urlObjects": [] }, () => { });
 
@@ -30,5 +13,7 @@ function createUrlObjects(details) {
     if (details.url.endsWith("m3u8") || details.url.includes("playlist.m3u8")) {
         let tempUrlObj = { "url": details.url };
         urlObjects.push(tempUrlObj);
+        
+        if (urlObjects.length > 5) urlObjects.pop();
     }
 }
--- src/script.js
+++ src/script.js
@@ -1,19 +1,54 @@
-const ul = document.getElementById("content-list");
-const syncBtn = document.getElementById("sync");
+const tippy = require("tippy.js");
 
-syncBtn.onclick = searchHAR;
+const ul = document.getElementById("content-list");
+const clear = document.getElementById("clear-icon");
+clear.onclick = clearStorage;
 
 function searchHAR() {
     chrome.storage.local.get("urlObjects", (result) => {
         if (result.urlObjects.length != 0) {
-
+            chrome.extension.getBackgroundPage().console.log(result.urlObjects);
+            document.getElementById("content-list").style.display = "block";
+            
             for (x in result.urlObjects) {
                 let li = document.createElement("li");
-                let sample = JSON.stringify(result.urlObjects[x].url);
+                li.classList.add("link");
+                tippy('#content-list', { target: ".link", content: "Click to copy" });
+                let sample = result.urlObjects[x].url;
                 li.appendChild(document.createTextNode(sample));
+                li.onclick = copyStringToClipboard(sample);
                 ul.appendChild(li);
             }
+        } else {
+            document.getElementById("nolinks").style.display = "block";
+            document.getElementById("content-list").style.display = "none";
         }
     });
-    
+
 }
+
+function clearStorage() {
+    chrome.storage.local.clear(() => {
+        let error = chrome.runtime.lastError;
+        if (error) chrome.extension.getBackgroundPage().console.log(error);
+    });
+    ul.innerHTML = '';
+    document.getElementById("nolinks").style.display = "block";
+    document.getElementById("content-list").style.display = "none";
+    chrome.storage.local.set({ "urlObjects": [] }, () => { });
+}
+
+function copyStringToClipboard(str) {
+    return function() {
+        var el = document.createElement('textarea');
+        el.value = str;
+        el.setAttribute('readonly', '');
+        el.style = { position: 'absolute', left: '-9999px' };
+        document.body.appendChild(el);
+        el.select();
+        document.execCommand('copy');
+        document.body.removeChild(el);
+    }
+}
+
+searchHAR();
